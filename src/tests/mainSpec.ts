3ef746090a65abf324272b9dd89e9ce72462e58b
amh_soft <amh_soft@outlook.com>
2022-03-09 11:57:43 +0200

    fix the test suit

--- src/tests/mainSpec.ts
+++ src/tests/mainSpec.ts
@@ -1,41 +1,62 @@
-import chai from 'chai';
-import chaiHttp from 'chai-http';
-import { SuperAgentRequest } from 'superagent';
+/* eslint-disable no-var */
+import Request from "request";
 
-chai.use(chaiHttp)
-
-const expect = chai.expect
-const req    = chai.request('http://localhost:8080/')
-
-it("Server is up and running", async () => {
-    const res = await tryCatch(req
-      .get('')
-    )
-    expect(res).to.have.status(200)
-  })
-
-it("should fail without parameters", async () => {
-    const res = await tryCatch(req
-      .get('resize')
-    )
-    expect(res).to.have.status(500)
-  })
-
-  it("should fail with invalid parameters", async () => {
-    const res = await tryCatch(req
-      .get('resize?image=encenadaport.jpg&width=aa&height=200')
-    )
-    expect(res).to.have.status(500)
-  })
-
-  it("should success with valid inputs", async () => {
-    const res = await tryCatch(req
-      .get('resize?image=encenadaport.jpg&width=200&height=200')
-    )
-    expect(res).to.have.status(200)
-  })
-
-  async function tryCatch(promise: SuperAgentRequest) {
-    try { return await promise }
-    catch(e) { return e }
-  }
+describe("Server", () => {
+    var server: { close: () => void; };
+    beforeAll(() => {
+        server = require("../main");
+    });
+    
+    describe("GET / Server is up and running", () => {
+        var data = {status:200,body:String};
+        beforeAll((done) => {
+            Request.get("http://localhost:8080/", (error, response, body) => {
+                data.status = response.statusCode;
+                data.body = body;
+                done();
+            });
+        });
+        it("Status 200", () => {
+            expect(data.status).toBe(200);
+        });
+        it("Body", () => {
+            expect(data.body).toBe("Welcome to Image Resizer App");
+        });
+    });
+    describe("GET /resize should success with valid inputs", () => {
+        var data = {status:200,body:String};
+        beforeAll((done) => {
+            Request.get("http://localhost:8080/resize?image=encenadaport.jpg&width=200&height=200", (error, response, body) => {
+                data.status = response.statusCode;
+                done();
+            });
+        });
+        it("Status 200", () => {
+            expect(data.status).toBe(200);
+        });
+    });
+    describe("GET /resize should fail without parameters", () => {
+      var data = {status:200,body:String};
+      beforeAll((done) => {
+          Request.get("http://localhost:8080/resize", (error, response, body) => {
+              data.status = response.statusCode;
+              done();
+          });
+      });
+      it("Status 200", () => {
+          expect(data.status).toBe(500);
+      });
+  });
+  describe("GET /resize should fail with invalid parameters", () => {
+    var data = {status:200,body:String};
+    beforeAll((done) => {
+        Request.get("http://localhost:8080/resize?image=encenadaport.jpg&width=aa&height=200", (error, response, body) => {
+            data.status = response.statusCode;
+            done();
+        });
+    });
+    it("Status 200", () => {
+        expect(data.status).toBe(500);
+    });
+});
+});
